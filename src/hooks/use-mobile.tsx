3c25ee440baf824b22ae01df00bc29a21efa6946
S MD Shabuddin Osaid <shabuddinaw@gmail.com>
2026-02-22 08:32:12 +0000

    not my open in new window preview is not working

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,17 +3,25 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    
+    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    // Set the initial value
+    setIsMobile(mql.matches)
+
+    // Listen for changes
+    mql.addEventListener('change', handleMediaQueryChange)
+
+    return () => {
+      mql.removeEventListener('change', handleMediaQueryChange)
+    }
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
